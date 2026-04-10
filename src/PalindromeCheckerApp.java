fbeb504754c23a5dc7a5091bb0338fcfc8fc7c71
poojagayathri-27 <pr7657@srmist.edu.in>
2026-04-10 08:46:59 +0530

    UC8: Linked List Based Palindrome Checker

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+public class PalindromeCheckerApp {
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node createLinkedList(String str) {
+        Node head = null, tail = null;
+        for (char ch : str.toCharArray()) {
+            Node newNode = new Node(ch);
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        return head;
+    }
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node nextNode;
+        while (current != null) {
+            nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+    public static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node secondHalf = reverse(slow.next);
+        Node firstHalf = head;
+        Node temp = secondHalf;
+        while (temp != null) {
+            if (firstHalf.data != temp.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String input = sc.nextLine().replaceAll("\\s+", "").toLowerCase();
+        Node head = createLinkedList(input);
+        if (isPalindrome(head)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+        sc.close();
+    }
+}
